95de50b92955a554d01006706807a47da00cc296
Nick Tomlin <nick.tomlin@gmail.com>
2015-09-10 17:02:53 -0500

    Add basic test

--- test/index.test.js
+++ test/index.test.js
@@ -9,8 +9,8 @@ test('Replaces "package.x" requires with data from package.json', function(t) {
   packageify(testFilePath)
     .on('data', function(d) { buffer += d })
     .on('end', function() {
-      t.ok(buffer.indexOf('1.0.3') > -1);
       t.ok(buffer.indexOf('Example Person') > -1);
+      t.ok(buffer.indexOf('1.0.3') > -1);
       t.end()
     })
     .end([
